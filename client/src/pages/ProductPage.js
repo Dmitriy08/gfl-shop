814790af7986c53a72093714b1e0ca18a65da07c
dmitr <dmitriy.pilyavskiy@gmail.com>
2021-06-08 23:32:01 +0300

    done 90% single product page

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Button, Col, Container, Image, ListGroup, Row} from "react-bootstrap";
 import {useParams} from 'react-router-dom'
 import ProductsApiService from "../services/products";
+
 const apiHost = process.env.REACT_APP_API_URL;
 
 const ProductPage = () => {
@@ -9,9 +10,18 @@ const ProductPage = () => {
     const [info, setInfo] = useState({})
     const [options, setOptions] = useState({})
 
+    const [type, setType] = useState(null)
+    const [color, setColor] = useState(null)
+    const [size, setSize] = useState(null)
+
     useEffect(() => {
         async function fetchData() {
-            const response = await ProductsApiService.getProduct(id);
+            console.log('render')
+            let query = ''
+            if (type) query += `type=${type}&`;
+            if (color) query += `color=${color}&`;
+            if (size) query += `size=${size}`;
+            const response = await ProductsApiService.getProduct(id, query);
             const data = await response.json()
             if (!response.ok) {
                 console.log('ERRRRROR', data)
@@ -20,19 +30,42 @@ const ProductPage = () => {
                 setOptions(data.options)
             }
         }
-        fetchData();
-    }, [id]);
 
-    console.log('info', info)
-    console.log('options', options)
+        fetchData();
+    }, [id, type, color, size]);
+
+    // console.log('info', info)
+    // console.log('options', options)
+
+    const clickTypeHandler = (id) => {
+        // console.log('TYPE_ID', id)
+        setType(id)
+    }
+
+    const clickColorHandler = (id) => {
+        // console.log('COLOR_ID', id)
+        setColor(id)
+    }
+
+    const clickSizeHandler = (id) => {
+        // console.log('SIZE_ID', id)
+        setSize(id)
+    }
+
     return (
         <section className="py-5">
             <Container>
                 <Row>
                     <Col lg={4}>
+                        {info.image_name &&
                         <Image src={apiHost + info.image_name}/>
+                        }
+
                     </Col>
                     <Col>
+                        <h5>Type: {type}</h5>
+                        <h5>Color: {color}</h5>
+                        <h5>Size: {size}</h5>
                         <h2>{info.product_name}</h2>
                         <p><strong>Category: </strong>{info.category_name}</p>
                         <p><strong>Structure: </strong>{info.structure_name}</p>
@@ -49,27 +82,46 @@ const ProductPage = () => {
                         <h4>Types</h4>
                         <ListGroup horizontal>
                             {
-                                info.type_name &&
-                                info.type_name.split(',').map((type, i) =>
-                                    <ListGroup.Item key={i}>{type}</ListGroup.Item>)
+                                info.id_type &&
+                                info.id_type.split(',').map((typeId, i) =>
+                                    <ListGroup.Item
+                                        active={typeId === type}
+                                        onClick={() => clickTypeHandler(typeId)}
+                                        key={i}
+                                    >
+                                        {info.type_name.split(',')[i]}
+                                    </ListGroup.Item>)
                             }
                         </ListGroup>
                         <hr/>
                         <h4>Colors</h4>
                         <ListGroup horizontal>
                             {
-                                options.color_name &&
-                                options.color_name.split(',').map((type, i) => <ListGroup.Item
-                                    key={i}>{type}</ListGroup.Item>)
+                                options.id_color &&
+                                options.id_color.split(',').map((colorId, i) =>
+                                    <ListGroup.Item
+                                        key={i}
+                                        active={!(colorId === color)}
+                                        style={{backgroundColor: options.color_code.split(',')[i]}}
+                                        onClick={() => clickColorHandler(colorId)}
+                                    >
+                                        {options.color_name.split(',')[i]}
+                                    </ListGroup.Item>)
                             }
                         </ListGroup>
                         <hr/>
                         <h4>Sizes</h4>
                         <ListGroup horizontal>
                             {
-                                options.size_name &&
-                                options.size_name.split(',').map((type, i) => <ListGroup.Item
-                                    key={i}>{type}</ListGroup.Item>)
+                                options.id_size &&
+                                options.id_size.split(',').map((sizeId, i) =>
+                                    <ListGroup.Item
+                                        active={sizeId === size}
+                                        key={i}
+                                        onClick={() => clickSizeHandler(sizeId)}
+                                    >
+                                        {options.size_name.split(',')[i]}
+                                    </ListGroup.Item>)
                             }
                         </ListGroup>
                     </Col>
--- client/src/services/products.js
+++ client/src/services/products.js
@@ -3,6 +3,6 @@ const apiHost = process.env.REACT_APP_API_URL;
 class ProductsApiService {
     getProducts = async () => await fetch(`${apiHost}api/products`);
 
-    getProduct = async (id) => await fetch(`${apiHost}api/products/${id}`)
+    getProduct = async (id, query = null) => await fetch(`${apiHost}api/products/${id}?${query}`)
 }
 export default new ProductsApiService();
